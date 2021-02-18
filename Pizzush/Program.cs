7acd932d6275a3411a3c556393faa8e34c4b3f85
Nikkx <nikolk2@gmail.com>
2021-02-18 20:09:10 +0200

    testing

--- Pizzush/Program.cs
+++ Pizzush/Program.cs
@@ -7,6 +7,7 @@ namespace Pizzush
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("MEOWWWWWWWWWWWW");
         }
     }
 }
